954d463f2d0042ab06f2cab1e8639a522a20f4a9
Ramona M <r1mari@yahoo.com>
2018-06-27 11:52:36 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(num)
+ 
+  if num= num/3
+    puts "Fizz"
+    
+    elsif num = num/5
+    puts "Buzz"
+    
+    elsif num = (num/3) && (num/5)
+    puts "FizzBuzz"
+    
+  else
+    puts "nil"
+    
+  end
+  
+end
