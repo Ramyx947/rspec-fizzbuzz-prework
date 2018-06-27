d4b910b398a772943317f8e87aae233f8c8780bc
Ramona M <r1mari@yahoo.com>
2018-06-27 12:27:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
-def fizzbuzz(fizz)
+def fizzbuzz(fizz_3, fizz_5, fizz_15)
 
-  if fizz % 3 == 0 && fizz % 5 == 0
+  if fizz_15 % 3 == 0 && fizz % 5 == 0
     puts 'FizzBuzz'
   
-  elsif fizz % 3 == 0
+  elsif fizz_3 % 3 == 0
     puts 'Fizz'
     
-  elsif fizz % 5 == 0
+  elsif fizz_5 % 5 == 0
     puts 'Buzz'
     
   else
