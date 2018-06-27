739caeb885c40e47775d99743b2ee62937fcf7b6
Ramona M <r1mari@yahoo.com>
2018-06-27 12:14:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 def fizzbuzz(n)
   if n%3==0 && n%5==0
     puts "FizzBuzz"
-  
+  end
   if n%3==0
     puts "Fizz"
-    
+  end
   if n%5==0
     puts "Buzz"
-    
-  else
-    puts n
   end
+  # else
+  #   puts n
+  # end
 end
   
