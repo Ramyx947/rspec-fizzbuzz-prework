cb5e8e23bcf5fa8846c0a7675d33b49732b4cf88
Ramona M <r1mari@yahoo.com>
2018-06-27 12:23:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(n)
   if (n % 3 == 0) && (n % 5 == 0)
-    puts "FizzBuzz"
+    puts 'FizzBuzz'
   
   elsif n%3==0
-    puts "Fizz"
+    puts 'Fizz'
     
   elsif n%5==0
-    puts "Buzz"
+    puts 'Buzz'
     
   else
     puts n
